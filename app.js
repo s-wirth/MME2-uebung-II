26d9b92822c29b121e9b467f49dacd3fe36086cd
Sophie <a.sophiewirth@gmail.com>
2015-10-29 13:11:56 +0100

    app.js serves any request with hello world

---
+++ app.js
@@ -0,0 +1,14 @@
+var express = require('express'),
+    app = express(),
+    port = process.env.PORT || 3000;
+
+
+app.use(function (req, res) {
+  var data = '<h1>hello world</h1>';
+
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  res.end(data)
+});
+app.listen(port);
+
+console.log('server started at port %s ', port);
