8b2a1934be0c099584708a75924bd5966dcc2165
Sophie <a.sophiewirth@gmail.com>
2015-10-29 13:53:56 +0100

    public prefix at url serves different content

--- app.js
+++ app.js
@@ -2,13 +2,20 @@ var express = require('express'),
     app = express(),
     port = process.env.PORT || 3000;
 
+app.use('/public', function (req, res) {
+    var data = '<h1>welcome to public</h1>';
 
-app.use(function (req, res) {
-  var data = '<h1>hello world</h1>';
-
-  res.writeHead(200, {'Content-Type': 'text/html'});
-  res.end(data)
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.end(data)
 });
+
+app.use('/', function (req, res) {
+    var data = '<h1>hello world</h1>';
+
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.end(data)
+});
+
 app.listen(port);
 
 console.log('server started at port %s ', port);
