f0b620e3ad1381da842ccd7f4308473fa9ae4bd2
Sophie <a.sophiewirth@gmail.com>
2015-10-29 17:57:27 +0100

    fixed mime type, put main back'

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 var express = require('express'),
     app = express(),
-    port = process.env.PORT || 3001;
+    port = process.env.PORT || 3001
+    path = require('path');
 
-app.use(express.static('public'));
-
+app.use('/public', express.static('public'));
+app.use('/', function (req, res) {
+    res.sendfile(path.join(__dirname + '/index.html'));
+});
 app.listen(port);
 
 console.log('Server started at http://localhost:%s/', port);
